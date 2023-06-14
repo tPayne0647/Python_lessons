87d19fa20f83a9ccf98a760aa619b845d20744d2
tpayne0647 <tristan.payne@northerndata.de>
2023-06-14 13:22:52 -0400

    renamed files

---
+++ Scripts/lucky_number_generator.py
@@ -0,0 +1,8 @@
+def lucky_number(name):
+    number = len(name) * 9
+    print("Hello ", name, "your lucky number is: ", str(number), "Have a good day! ")
+    
+
+lucky_number("Tristan")
+lucky_number("Dax")
+lucky_number("Meredith")
