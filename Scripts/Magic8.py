26813bb3168a40c26a30256876988768d52a4ea6
tpayne0647 <tristan.payne@northerndata.de>
2023-06-20 15:46:08 -0400

    changed output

--- Scripts/Magic8.py
+++ Scripts/Magic8.py
@@ -6,35 +6,35 @@ answer = ""
 random_number = random.randint(1, 9)
 
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "Yes - definitely!"
 
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = "It is decidedly so..."
 
 elif random_number == 3:
-  answer = "Without a doubt"
+  answer = "Without a doubt!"
 
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = "Reply hazy, try again..."
 
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = "Ask again later..."
 
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = "Better not tell you now."
 
 elif random_number == 7:
-  answer = "Sorry " + name + ", My sources say no"
+  answer = "Sorry " + name + ", My sources say no."
 
 elif random_number == 8:
-  answer = "Sorry " + name + ", Outlook not so good"
+  answer = "Sorry " + name + ", Outlook not so good..."
 
 elif random_number == 9:
-  answer = "Sorry " + name + ", Very doubtful"
+  answer = "Sorry " + name + ", Very doubtful..."
 else:
-  print("Error")
+  print("Error!!!")
 
-print(name + "asks: " + question)
+
 print(""" 
       ____
     ,dP9CGG88@b,
@@ -50,4 +50,6 @@ Y8GGGGGG8888888@@@@P.....
     `@@@@@@@@@P'.......
         '''''........                   
       """)
-print("Magic 8-Ball says: " + answer)
+print(name + ' asks: ' +'"'   + question + '"')
+print("*******************************************************************************")
+print("Magic 8-Ball's answer: " + answer)
