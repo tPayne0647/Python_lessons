9cd54c4dbeb1b00e2bbb66f54080d7d9bbfb6928
tpayne0647 <tristan.payne@northerndata.de>
2023-06-21 15:54:23 -0400

    cleaned up code a bit

--- sort.py
+++ sort.py
@@ -1,4 +1,10 @@
+# Sort using sort
 cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
 cities = cities.sort()
 print(cities)
 
+
+# sort using sorted
+states = ["Florida", "Georgia", "Colorado", "Texas", "Virginia"]
+states = sorted(states)
+print(states)
