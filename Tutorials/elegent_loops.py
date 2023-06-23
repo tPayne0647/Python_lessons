9543349d851dddf120dec7579346191697a23c90
tpayne0647 <tristan.payne@northerndata.de>
2023-06-23 17:05:33 -0400

    spellcheck test

--- Tutorials/elegent_loops.py
+++ Tutorials/elegent_loops.py
@@ -1,13 +1,17 @@
-# # Add 10 points to each grade and print resulting grades
-# grades = [90, 88, 62, 76, 74, 89, 48, 57]
-# scaled_grades = [grade + 10 for grade in grades]
-# print(scaled_grades)
+"""
+This module provides examples of elegant loop constructs in Python.
+"""
+
+# Add 10 points to each grade and print resulting grades
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_grades = [grade + 10 for grade in grades]
+print(scaled_grades)
 
 
-# # Double list of numbers and print out both lists using only 3 lines
-# numbers = [2, 4, 10, 100, 500, 1000]
-# doubled = [i * 2 for i in numbers]
-# print(str(numbers) + "\n" + str(doubled))
+# Double list of numbers and print out both lists using only 3 lines
+numbers = [2, 4, 10, 100, 500, 1000]
+doubled = [i * 2 for i in numbers]
+print(str(numbers) + "\n" + str(doubled))
 
 
 # List comprehension: conditional - can only ride if taller than 161
@@ -15,3 +19,4 @@ heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 
 can_ride_coaster = [i for i in heights if i > 161]
 print(can_ride_coaster)
+print("Is my speltchekcer workcing?")
