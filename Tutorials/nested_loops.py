49115226d780ff222126047630e8a3639c5280cf
tpayne0647 <tristan.payne@northerndata.de>
2023-06-23 10:24:36 -0400

    added tutorial work

---
+++ Tutorials/nested_loops.py
@@ -0,0 +1,9 @@
+sales_data = [[102, 342, 97], [98, 43, 110], [400, 123, 666]]
+total_sales = 0
+
+for locations in sales_data:
+    print(locations)
+    for i in locations:
+        total_sales += i
+        
+print(total_sales)
