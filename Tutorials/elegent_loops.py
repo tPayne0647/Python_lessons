8567ea78d1b190e4945d53434d3a4972b734c71d
tpayne0647 <tristan.payne@northerndata.de>
2023-06-23 16:56:07 -0400

    spellcheck test

--- Tutorials/elegent_loops.py
+++ Tutorials/elegent_loops.py
@@ -1,13 +1,13 @@
-# Add 10 points to each grade and print resulting grades
-grades = [90, 88, 62, 76, 74, 89, 48, 57]
-scaled_grades = [grade + 10 for grade in grades]
-print(scaled_grades)
+# # Add 10 points to each grade and print resulting grades
+# grades = [90, 88, 62, 76, 74, 89, 48, 57]
+# scaled_grades = [grade + 10 for grade in grades]
+# print(scaled_grades)
 
 
-# Double list of numbers and print out both lists using only 3 lines
-numbers = [2, 4, 10, 100, 500, 1000]
-doubled = [i * 2 for i in numbers]
-print(str(numbers) + "\n" + str(doubled))
+# # Double list of numbers and print out both lists using only 3 lines
+# numbers = [2, 4, 10, 100, 500, 1000]
+# doubled = [i * 2 for i in numbers]
+# print(str(numbers) + "\n" + str(doubled))
 
 
 # List comprehension: conditional - can only ride if taller than 161
